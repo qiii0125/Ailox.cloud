ed2a0e40c2771e9db8899b2bcde38be6deaaf012
qiii0125 <wiratamafadhli@gmail.com>
2023-01-17 16:32:27 +0700

    revisi

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,14 @@ module.exports = {
         primary: "#14b8a6",
         dark: "#0f172a",
         secondary: "#64748b",
+        client: "#bfbfd8"
       },
       screens: {
         '2xl': '1230px',
-      }
+      },
+      backgroundImage: {
+        'hero-pattern': "url('dist/img/1.png')",
+      },
     },
   },
   plugins: [],
