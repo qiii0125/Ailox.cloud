04857f0a8f41e9533936a171ffd16aa95d3bd0c2
qiii0125 <wiratamafadhli@gmaail.com>
2023-02-08 15:46:33 +0700

    revisi

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
         primary: "#14b8a6",
         dark: "#0f172a",
         secondary: "#64748b",
-        client: "#bfbfd8"
+        client: "#bfbfd8",
+        latar: "#ccd1d1",
+        latar2: "#e2e8f0",
+        latar3: "#cbd5e1",
       },
       screens: {
         '2xl': '1230px',
